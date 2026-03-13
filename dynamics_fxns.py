f848f759738c7b96045f3f8c9bb44960fd62fe1b
anishsarma <62362168+anishsarma@users.noreply.github.com>
2026-03-12 22:10:22 -0700

    Add files via upload

--- dynamics_fxns.py
+++ dynamics_fxns.py
@@ -2,7 +2,7 @@ import copy
 import numpy as np
 from physiology_utilities import computeFlows, dictToVec, vecToDict, dictToGraph, graphToDict
 
-def odemasterfxn(t, state, speciesList,compartmentList, k, externalK, externalW, growth,carrierDict):
+def odemasterfxn(t, state, speciesList,compartmentList, k, externalK, growth,carrierDict):
     # Unpack the state variables using vecToDict
     stateDict = vecToDict(state,speciesList,compartmentList)
     # dynamics: call the modular flowDynamics function for each species
@@ -11,26 +11,25 @@ def odemasterfxn(t, state, speciesList,compartmentList, k, externalK, externalW,
     # Re-order so that we don't need two compartment-list loops here
     netflows = {}
     for sp in speciesList:
-        x = stateDict[sp]
+        xsp = stateDict[sp]
+        x = stateDict
         dspecies = {}
         netflows[sp] = {}
-        if not carrierDict[sp]:
-            for cpt in compartmentList:
-                insystem_inflows = 0
-                insystem_outflows = 0
-                for key in k[cpt].keys(): # for each compartment
-                    # this compartment to someplace
-                    insystem_outflows+=k[cpt][key]*x[cpt]
-                    # someplace to this compartment 
-                    insystem_inflows+=k[key][cpt]*x[key]
-                netflows[sp][cpt] = insystem_inflows-insystem_outflows
-                dspecies[cpt] = growth[sp][cpt]*x[cpt]+netflows[sp][cpt]+externalK[sp][cpt](t,x)*x[cpt]+externalW[sp][cpt](t,x)
-        else:
-            carriersp = carrierDict[sp]
-            netflows[sp] = netflows[carriersp]
-            for cpt in compartmentList:
-                # TODO: make this cleaner
-                cV = stateDict[carriersp][cpt]
-                dspecies[cpt] = (growth[sp][cpt]*x[cpt]*cV+netflows[sp][cpt]*cV+externalK[sp][cpt](t,x)*x[cpt]*cV+externalW[sp][cpt](t,x)*cV)/cV
-        diffDict[sp] = dspecies
+        for cpt in compartmentList:
+            insystem_inflows = 0
+            insystem_outflows = 0
+            for key in k[cpt].keys(): # for each compartment
+                # this compartment to someplace
+                new_out = k[cpt][key]*xsp[cpt]
+                new_in = k[key][cpt]*xsp[key]
+                carrierKey = carrierDict[sp]
+                while carrierKey:
+                    new_out*=stateDict[carrierKey][cpt]
+                    new_in*=stateDict[carrierKey][key]
+                    carrierKey = carrierDict[carrierKey]
+                insystem_outflows+=new_out
+                insystem_inflows+=new_in
+            netflows[sp][cpt] = insystem_inflows-insystem_outflows
+            dspecies[cpt] = growth[sp][cpt]*xsp[cpt]+netflows[sp][cpt]+externalK[sp][cpt](t,x)
+            diffDict[sp] = dspecies
     return dictToVec(diffDict,speciesList,compartmentList)
